d157d363becc04a91318960b59f4d0da0b49f742
Aleksander Siudek <alek@siudek.net>
2022-05-20 15:01:11 +0200

    WIP

---
+++ project1/program.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main(){
+   setlocale(LC_ALL,"Polish");
+   int wybor;
+    cout<< "wybierz 1 - tworzenie pliku" <<endl;
+     cout<< "wybierz 2 - wpisz do pliku"<< endl;
+      cout<< "wybierz 3 - ile wpłacone" <<endl;
+       cout<< "wybierz 4 - odczytaj zawartosc pliku "<< endl;
+       cout<< "wybierz 5 -kasowanie pliku" <<endl;
+       cout<< "wybierz 6- odczytaj po nazwisku"<<endl <<"dokonaj wyboru:";
+       cin>>wybor;
+
+       if(wybor == 1);
+       fstream plik;
+       plik.open("baza.txt",ios::in);
+       if(plik.is_open())
+       {
+          cout<<"plik jest jest ";
+          plik.close();
+          return true;
+          }
+          else{
+          cout<<"nie ma pliku - utworzy go";
+          ofstream utworz("baza.txt");
+
+          return false;
+          }
+       }
+
+if(wybor ==2){
+   ofstream wpisz;//("baza.txt");
+   wpisz.open("baza.txt",ios::out(ios::in)
+   wpisz.seekp(0,ios::end);
+   string imie; int wplata;
+   cout<<"podaj imie" <<endl;
+   cin>>imie;
+   cout <<"podaj wplate" <<endl;
+   cin>>wplata;
+
+   wpisz << imie << " "  <<wplata<<endl;
+}
+else{
+   //cout<<"BLAD!!: nie mozna otworzyc pliku."<<endl;
+   
+}
+if(wybor==  4){
+   ifstream czytaj;
+   czytaj.open("baza.txt");
+   if(czytaj.good());
+}
